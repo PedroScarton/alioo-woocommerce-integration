5541b9802c1ada61410cfa618129f18117439df7
Pedro Scarton <43622582+PedroScarton@users.noreply.github.com>
2024-10-17 18:06:48 -0300

    enabling just single product sync in woocommerce

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from config.settings import wcapi
 from modules.excel_download import download_excel
 from modules.read_excel import read_excel
 from modules.woocommerce_api import get_all_woocommerce_products
-from modules.product import prepare_dataframes, identify_products, separate_simple_and_variable, format_simple_products, format_variable_products, format_updated_simple_products, map_sku_to_id, format_updated_variable_products
+from modules.product import pre_process_df, identify_products, separate_simple_and_variable, format_simple_products, format_variable_products, format_updated_simple_products, map_sku_to_id, format_updated_variable_products
 from modules.woocommerce_api import create_simple_products, create_variable_products, update_products, delete_products_batch, update_variable_products, get_all_woocommerce_categories, create_missing_categories
 from modules.dataframe_operations import get_unique_categories
 
@@ -35,9 +35,9 @@ def main():
         df_wc = get_all_woocommerce_products(wcapi)
 
         # Step 4: Read WooCommerce products
-        df_excel, df_wc = prepare_dataframes(df_excel, df_wc)
+        df_excel = pre_process_df(df_excel)
 
-        # Step 5: Identify new, updated, and deleted products
+        # # Step 5: Identify new, updated, and deleted products
         df_new, df_updated, df_delete = identify_products(df_excel, df_wc)
 
         # Extract unique categories from Excel
@@ -49,53 +49,48 @@ def main():
         # Create missing categories
         category_name_to_id = create_missing_categories(wcapi, category_names, category_name_to_id)
 
-        # Proceed with identifying new, updated, and deleted products
-        df_new, df_updated, df_delete = identify_products(df_excel, df_wc)
-
         if not df_new.empty:
             logging.info(f"Found {len(df_new)} new products.")
             # Step 6: Separate simple and variable products
             df_new_simple, df_new_variable = separate_simple_and_variable(df_new)
 
-            df_new_simple.to_csv('data/new_simple.csv', index=False)
-
             # Format and create new products
             new_simple_products = format_simple_products(df_new_simple, category_name_to_id)
-            new_variable_products = format_variable_products(df_new_variable, category_name_to_id)
+            # new_variable_products = format_variable_products(df_new_variable)
 
             # Step 7: Create new products in WooCommerce
             create_simple_products(wcapi, new_simple_products)
-            create_variable_products(wcapi, new_variable_products)
+            # create_variable_products(wcapi, new_variable_products)
 
-        # If there are products to update in WooCommerce (df_updated is not empty)
-        if not df_updated.empty:
-            logging.info(f"Found {len(df_updated)} products to update.")
-            # Step 9: Map SKU to WooCommerce ID
-            sku_to_id = map_sku_to_id(df_wc)
+        # # If there are products to update in WooCommerce (df_updated is not empty)
+        # if not df_updated.empty:
+        #     logging.info(f"Found {len(df_updated)} products to update.")
+        #     # Step 9: Map SKU to WooCommerce ID
+        #     sku_to_id = map_sku_to_id(df_wc)
 
-            # Step 10: Process updated products
-            df_updated_simple, df_updated_variable = separate_simple_and_variable(df_updated)
+        #     # Step 10: Process updated products
+        #     df_updated_simple, df_updated_variable = separate_simple_and_variable(df_updated)
 
-            # Step 11: Format products
-            updated_simple_products = format_updated_simple_products(df_updated_simple, sku_to_id, category_name_to_id)
-            updated_variable_products = format_updated_variable_products(df_updated_variable, sku_to_id, category_name_to_id, wcapi)
+        #     # Step 11: Format products
+        #     updated_simple_products = format_updated_simple_products(df_updated_simple, sku_to_id, category_name_to_id)
+        #     updated_variable_products = format_updated_variable_products(df_updated_variable, sku_to_id, category_name_to_id, wcapi)
 
 
-            # Step 12: Update products in WooCommerce
-            update_products(wcapi, updated_simple_products)
-            update_variable_products(wcapi, updated_variable_products)
-            logging.info("Products updated.")
-        else:
-            logging.info("No products to update.")
+        #     # Step 12: Update products in WooCommerce
+        #     update_products(wcapi, updated_simple_products)
+        #     update_variable_products(wcapi, updated_variable_products)
+        #     logging.info("Products updated.")
+        # else:
+        #     logging.info("No products to update.")
 
-        if not df_delete.empty:
-            logging.info(f"Found {len(df_delete)} products to delete.")
-            # Step 13: Delete products in WooCommerce that are not in the Excel file
-            product_ids_to_delete = df_delete['id'].tolist()
-            delete_products_batch(wcapi, product_ids_to_delete)
-            logging.info("Products deleted.")
-        else:
-            logging.info("No products to delete.")
+        # if not df_delete.empty:
+        #     logging.info(f"Found {len(df_delete)} products to delete.")
+        #     # Step 13: Delete products in WooCommerce that are not in the Excel file
+        #     product_ids_to_delete = df_delete['id'].tolist()
+        #     delete_products_batch(wcapi, product_ids_to_delete)
+        #     logging.info("Products deleted.")
+        # else:
+        #     logging.info("No products to delete.")
 
         logging.info("Process finished successfully.")
 
